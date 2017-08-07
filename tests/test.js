adec0cf49e8fd62bc6730dfd3c71aced23e79823
dates <jake.brooks@midsummerish.com>
2017-08-07 12:01:04 -0400

    eslint gets what eslint wants

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,9 @@
-import Signal from '../dist/Signal.js';
 import test from 'ava';
+import Signal from '../dist/Signal';
 
-
-
-
-
-test('foo', t => {
+test('foo', (t) => {
   const signal = new Signal();
-  return signal.version().then( (v) => {
-     console.log(v);
-     t.is(v.replace(/\s+/g, ' ').trim(), 'signal-cli 0.5.5');
-   });
-
+  return signal.version().then((v) => {
+    t.is(v.replace(/\s+/g, ' ').trim(), 'signal-cli 0.5.5');
+  });
 });
-
-// test('bar', async t => {
-// 	const bar = Promise.resolve('bar');
-//
-// 	t.is(await bar, 'bar');
-// });
